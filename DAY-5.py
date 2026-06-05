c0f5a311302c68dd5d1197b815ccec63255f1b08
NAGENDRASAIBALAJI <nagendrasaibalaji@gmail.com>
2026-06-05 15:42:31 +0530

    Add files via upload

---
+++ DAY-5.py
@@ -0,0 +1,112 @@
+'''
+MODULES:
+--------
+--> A modules in python is a file that contains python code such as
+-variables
+-functions
+-classes
+-statements
+
+
+two type of modules
+--------------------------
+user- define
+built-in
+-------------
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+---------------
+import math
+print(math.sqrt(25))
+
+print(math.factorial(10))
+print(math.pow(2,5))
+----------------------
+
+import os
+os.remove("deo.txt")
+----------------------
+
+import sys
+print(sys.version)
+print(sys.exit)
+print(sys.path)
+
+------------------
+import random
+print(random.random())
+----------------------
+
+import collections import counter
+data = ['a','b','c','d']
+counter = counter(data)
+print(counter)
+----------------------------- 
+dd = defaultdict(int)
+dd['missing'] += 1
+print(dd['missing'])
+print(dd)
+
+-------------------------------
+is,==
+mutable,immutable
+extend,append
+memory allocation in python
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
